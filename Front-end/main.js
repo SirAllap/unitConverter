7be7bef7e4385c6f35a06ad49f6ec0242c4a24c5
David Pallarés <serallapdivad@gmail.com>
2023-07-03 12:39:11 +0100

    Small changes typo

--- Front-end/main.js
+++ Front-end/main.js
@@ -2,12 +2,12 @@ const app = new Vue({
     el: "#main-container",
     data: {
         conversions: [
-            { unit: "Km → Miles", fromUnit: "km", val: "km2mil" },
-            { unit: "Miles → Km", fromUnit: "Miles", val: "mil2km" },
-            { unit: "Pies → Metros", fromUnit: "Pies", val: "pies2m" },
-            { unit: "Metros → Pies", fromUnit: "Metros", val: "m2pies" },
-            { unit: "Cm → Pulgadas", fromUnit: "Cm", val: "cm2pul" },
-            { unit: "Pulgadas → Cm", fromUnit: "Pulgadas", val: "pul2cm" },
+            { unit: "Kilometer → Mile", fromUnit: "km", val: "km2mil" },
+            { unit: "Mile → Kilometer", fromUnit: "miles", val: "mil2km" },
+            { unit: "Foot → Meter", fromUnit: "foot", val: "pies2m" },
+            { unit: "Meter → Foot", fromUnit: "meters", val: "m2pies" },
+            { unit: "Centimeter → Inch", fromUnit: "cm", val: "cm2pul" },
+            { unit: "Inch → Centimeter", fromUnit: "inch", val: "pul2cm" },
         ],
         savedConversions: [],
         inputNumber: 0,
@@ -31,53 +31,38 @@ const app = new Vue({
             if (this.inputOption === "km2mil" && this.inputNumber != 0) {
                 this.errorMsg = ""
                 this.unitToChange = "km"
-                this.unitChanged = "miles"
+                this.unitChanged = "mi"
                 return (this.resultOfConversion = (this.inputNumber * 0.6213711922).toFixed(2))
             } else if (this.inputOption === "mil2km" && this.inputNumber != 0) {
                 this.errorMsg = ""
-                this.unitToChange = "miles"
+                this.unitToChange = "mi"
                 this.unitChanged = "km"
                 return (this.resultOfConversion = (this.inputNumber * 1.60934).toFixed(2))
             } else if (this.inputOption === "pies2m" && this.inputNumber != 0) {
                 this.errorMsg = ""
-                this.unitToChange = "pies"
-                this.unitChanged = "metros"
+                this.unitToChange = "ft"
+                this.unitChanged = "m"
                 return (this.resultOfConversion = (this.inputNumber * 0.3048).toFixed(2))
             } else if (this.inputOption === "m2pies" && this.inputNumber != 0) {
                 this.errorMsg = ""
-                this.unitToChange = "metros"
-                this.unitChanged = "pies"
+                this.unitToChange = "m"
+                this.unitChanged = "ft"
                 return (this.resultOfConversion = (this.inputNumber * 3.28084).toFixed(2))
             } else if (this.inputOption === "cm2pul" && this.inputNumber != 0) {
                 this.errorMsg = ""
-                this.unitToChange = "centímetros"
-                this.unitChanged = "pulgadas"
+                this.unitToChange = "cm"
+                this.unitChanged = "in"
                 return (this.resultOfConversion = (this.inputNumber * 0.393701).toFixed(2))
             } else if (this.inputOption === "pul2cm" && this.inputNumber != 0) {
                 this.errorMsg = ""
-                this.unitToChange = "pulgadas"
-                this.unitChanged = "centímetros"
+                this.unitToChange = "in"
+                this.unitChanged = "cm"
                 return (this.resultOfConversion = (this.inputNumber * 2.54).toFixed(2))
             } else {
                 this.resultOfConversion = 0
                 this.unitChanged = ""
                 return this.errorMsg = "!"
             }
-        },
-        addConversion() {
-            if (!this.inputOption) return;
-            this.savedConversions.push({ currentUnit: this.inputNumber, unit2change: this.unitToChange, convertedUnit: this.resultOfConversion, unitChanged: this.unitChanged });
-            // Clean
-            // this.inputOption = '';
-            this.saveConversion();
-        },
-        removeSavedConversions(x) {
-            this.savedConversions.splice(x, 1);
-            this.saveConversion();
-        },
-        saveConversion() {
-            let parsed = JSON.stringify(this.savedConversions);
-            localStorage.setItem('savedConversions', parsed);
         },
         swapUnit() {
             if (this.inputOption === "km2mil") {
@@ -95,13 +80,24 @@ const app = new Vue({
             } else if (this.inputOption === "cm2pul") {
                 this.inputOption = "pul2cm"
                 this.convert();
-            } else if (this.inputOption === "pul2cm") {
+            } else {
                 this.inputOption = "cm2pul"
                 this.convert();
-            } else {
-
             }
-        }
+        },
+        addConversion() {
+            if (!this.inputOption) return;
+            this.savedConversions.push({ currentUnit: this.inputNumber, unit2change: this.unitToChange, convertedUnit: this.resultOfConversion, unitChanged: this.unitChanged });
+            this.saveConversion();
+        },
+        removeSavedConversions(x) {
+            this.savedConversions.splice(x, 1);
+            this.saveConversion();
+        },
+        saveConversion() {
+            let parsed = JSON.stringify(this.savedConversions);
+            localStorage.setItem('savedConversions', parsed);
+        },
     },
 });
 
